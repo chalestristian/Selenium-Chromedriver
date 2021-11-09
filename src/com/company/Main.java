56cba9fe5c8efc6aae930a496f617af731c1fd08
Thales Cristian <thaleseugenio.3012@aluno.unibh.br>
2021-11-08 22:06:59 -0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        abrirChrome();
+    }
+    private static void abrirChrome() {
+        File file = new File("/home/chalestristian/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        WebDriver driver = new ChromeDriver();
+        driver.get("hTp://devmedia.com.br");
+        String i = driver.getCurrentUrl();
+        System.out.println(i);
+        driver.close();
+    }
+}
